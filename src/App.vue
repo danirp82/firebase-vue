2babfecdb0c7f726f6e043a64d87e01aefc9da36
Daniel Rodriguez <dani.rp12682@gmail.com>
2020-12-30 12:47:05 +0100

    Add name and date in messages, add transition and refactor getRoom

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <NavBarComponent />
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -13,25 +15,13 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/store/rooms.js
+++ src/store/rooms.js
@@ -75,11 +75,21 @@ const actions = {
       commit("setLoading", false, { root: true });
     }
   },
-  async getRoom(context, roomID) {
-    return await db
-      .collection("rooms")
-      .doc(roomID)
-      .get();
+  async getRoom({ getters }, roomID) {
+    // Grab from local state
+    let room = getters["getRoom"](roomID);
+    if (!room) {
+      // Grab from Cloud Firestore 🔥
+      room = await db
+        .collection("rooms")
+        .doc(roomID)
+        .get();
+
+      if (!room.exists) throw new Error("Could not find room");
+      room = room.data();
+    }
+
+    return room;
   },
   async updateRoom(context, { roomID, name, description }) {
     const roomData = {};
